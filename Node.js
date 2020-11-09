f03b3f3b268707c203d5e55d54499f39e38d179a
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-09 13:45:30 +0100

    ertw

---
+++ Node.js
@@ -0,0 +1,11 @@
+let http = require('http');
+// process.env.PORTletsthe port beset by Heroku
+let port = process.env.PORT|| 8080;
+
+http.createServer(function(request, response) {
+    response.writeHead(200);
+    response.write("HelloWorld!");
+    response.end();
+}).listen(port);
+
+console.log("Listeningon port " + port + "...");
