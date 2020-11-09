acb8be74d407aa22c0701c8fd6da2d775aaa9ae3
SimonDalle <simondalsgaard9@live.dk>
2020-11-09 14:08:02 +0100

    Simon push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let port = process.env.PORT|| 8080;
 
 http.createServer(function(request, response) {
     response.writeHead(200);
-    response.write("Hello World!");
+    response.write("Hello World and others!");
     response.end();
 }).listen(port);
 
