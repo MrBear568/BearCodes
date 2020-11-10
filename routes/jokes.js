45c73d0201b2ad9995ece1028be0d580b3feefb1
SimonDalle <simondalsgaard9@live.dk>
2020-11-10 08:41:32 +0100

    jokes routes oprettet

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,2 +1,20 @@
+const controller = require("../controllers/controller");
 const express = require('express');
 const router = express.Router();
+
+router
+    .get('/api/jokes', async (req, res) => {
+
+    })
+    .get('/api/othersites', async (req, res) => {
+
+    })
+    .get('/api/otherjokes/:site', async (req, res) => {
+
+    })
+    .post('/api/jokes', async (req, res) => {
+
+    })
+
+
+module.exports = router;
