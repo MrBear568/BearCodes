017b9673ea780be2c2b27e1d14205607a4dab8f1
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-11 09:49:04 +0100

    jokes route, get og post ændret

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,13 +1,20 @@
 const controller = require("../controllers/controller");
 const express = require('express');
+const jokes = require("../models/jokes");
 const router = express.Router();
 
 router
     .get('/api/jokes', async (req, res) => {
+        const jokes = await controller.getJokes()
 
+        res.render('jokes', {jokes: jokes})
     })
     .post('/api/jokes', async (req, res) => {
+        const setup = req.body.setup
+        const punchline = req.body.punchline
+        controller.createJoke(setup, punchline)
 
+        res.redirect('/api/jokes')
     })
 
 //Dette er en test 
