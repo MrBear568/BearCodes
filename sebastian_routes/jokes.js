0d61a8834f1fef73fbdffe21f2950a40080ecf69
Sebastian Rom <eaasrvh@students.eaaa.dk>
2020-11-11 09:21:12 +0100

    routes delt op

--- sebastian_routes/jokes.js
+++ sebastian_routes/jokes.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 router
-    .get('/api/jokes', async (req, res) => {
+    .get('/api/othersites', async (req, res) => {
 
     })
-    .post('/api/jokes', async (req, res) => {
+    .get('/api/otherjokes/:site', async (req, res) => {
 
     })
 
