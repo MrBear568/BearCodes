6bb3f6a0e8adeec14eee3d088c038c6497c4d23c
SimonDalle <simondalsgaard9@live.dk>
2020-11-11 08:24:14 +0100

    controller oprettet

---
+++ controllers/controller.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const jokes = require('../models/jokes');
+
+mongoose.connect(config.databaseURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+exports.createJoke = function (setup, punchline) {
+    return jokes.create({
+        setup,
+        punchline
+    });
+};
+
+exports.getJokes = function () {
+    return jokes.find().populate('').exec();
+};
