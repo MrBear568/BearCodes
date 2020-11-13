9b41ef6b98b1371a439292c7f3a3d74c310243c3
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-13 11:48:03 +0100

    We did it baby!

--- app.js
+++ app.js
@@ -2,30 +2,34 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 const config = require('./config');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const bodyparser = require('body-parser');
 app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-console.log(config.mongoDBHost);
-mongoose.connect(config.mongoDBHost, {
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    autoIndex: true, 
+    autoIndex: true,
     useUnifiedTopology: true
 });
-
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('Connected to mongoose'))
 
 //Routes 
 const jokesRoute = require('./routes/jokes');
 app.use('/', jokesRoute);
 app.use('/api/othersites', jokesRoute);
-//app.use('/api/jokes', jokesRoute);
+app.use('/api/jokes', jokesRoute);
 
-app.listen(process.env.PORT, config.localPort, () =>{
-    console.log(`server kører på port ${config.localPort}`);
+app.listen(process.env.PORT, () => {
+    console.log(`server kører på port ${process.env.PORT}`);
 })
 
 
