bdeb6ef62361494912a92d1c851b782d01febd16
Bjoern Petersen Werner <bjrn.werner@gmail.com>
2020-11-16 11:46:26 +0100

    back to the future

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyparser.urlencoded({extended: false}));
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb+srv://admin:<admin>@bearcluster.wat3l.mongodb.net/<BearJokes>?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     autoIndex: true, 
